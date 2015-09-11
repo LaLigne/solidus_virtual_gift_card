3c6275681e8861992391fdcaaac3f1d583e23a00
Dmitri Vassilev <dmitrivassilev@gmail.com>
2015-09-11 16:47:20 -0400

    inverting sort order

--- app/controllers/spree/admin/gift_cards_controller.rb
+++ app/controllers/spree/admin/gift_cards_controller.rb
@@ -3,7 +3,7 @@ class Spree::Admin::GiftCardsController < Spree::Admin::BaseController
   before_filter :load_user, only: [:lookup ]
 
   def index
-    @gift_cards = Spree::VirtualGiftCard.page(params[:page])
+    @gift_cards = Spree::VirtualGiftCard.order(id: :desc).page(params[:page])
   end
 
   def show
